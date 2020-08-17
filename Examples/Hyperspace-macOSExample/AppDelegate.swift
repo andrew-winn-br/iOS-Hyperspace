62cd2bdeb7d65331ed11f2b1de56e7d6895e73b5
Andrew Winn <andrew.winn@bottlerocketstudios.com>
2020-08-17 15:58:54 -0500

    Add AppKit app

---
+++ Examples/Hyperspace-macOSExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hyperspace-macOSExample
+//
+//  Created by Andrew Winn on 8/17/20.
+//  Copyright © 2020 Bottle Rocket Studios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Examples/Hyperspace-macOSExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hyperspace-macOSExample
+//
+//  Created by Andrew Winn on 8/17/20.
+//  Copyright © 2020 Bottle Rocket Studios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
