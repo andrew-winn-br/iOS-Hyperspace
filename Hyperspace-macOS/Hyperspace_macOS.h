0c68132a3bac2181cf6e3fc9fe84fe5a430a1854
Andrew Winn <andrew.winn@bottlerocketstudios.com>
2020-08-19 15:51:47 -0500

    Added framework target

---
+++ Hyperspace-macOS/Hyperspace_macOS.h
@@ -0,0 +1,19 @@
+//
+//  Hyperspace_macOS.h
+//  Hyperspace-macOS
+//
+//  Created by Andrew Winn on 8/19/20.
+//  Copyright © 2020 Bottle Rocket Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Hyperspace_macOS.
+FOUNDATION_EXPORT double Hyperspace_macOSVersionNumber;
+
+//! Project version string for Hyperspace_macOS.
+FOUNDATION_EXPORT const unsigned char Hyperspace_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hyperspace_macOS/PublicHeader.h>
+
+
